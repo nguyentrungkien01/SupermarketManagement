f4d920e2af8db9abe2953d808cbe5cc772e43f02
vantruong2220 <1951052220truong@gmail.com>
2022-04-03 11:51:35 +0700

    sign in

--- SupermarketManagement/src/main/java/com/ou/controllers/App.java
+++ SupermarketManagement/src/main/java/com/ou/controllers/App.java
@@ -17,7 +17,7 @@ public class App extends Application {
 
     @Override
     public void start(Stage stage) throws IOException {
-        scene = new Scene(loadFXML("branch"));
+        scene = new Scene(loadFXML("signIn"));
         stage.setScene(scene);
         stage.show();
     }
---
+++ SupermarketManagement/src/test/java/com/ou/services/SignInServiceTest.java
@@ -0,0 +1,120 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.ou.services;
+
+import com.ou.services.SignInService;
+import com.ou.utils.DatabaseUtils;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
+import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.BeforeAll;
+import com.ou.pojos.Staff;
+import java.security.NoSuchAlgorithmException;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+/**
+ *
+ * @author Admin
+ */
+public class SignInServiceTest {
+    private static Connection conn;
+    private static SignInService signInService;
+    
+    public  SignInServiceTest(){
+        
+    }
+      
+    @BeforeAll
+    public static void beforeAll() {
+        try {
+            conn = DatabaseUtils.getConnection();
+        } catch (SQLException ex) {
+            ex.printStackTrace();
+        }
+        
+        signInService = new SignInService();
+    }
+    
+    @AfterAll
+    public static void afterAll() {
+        if (conn != null)
+            try {
+                conn.close();
+            } catch (SQLException ex) {
+                System.out.println(ex);
+            }
+    }
+    
+    @BeforeEach
+    public void setUp() {
+    }
+
+    @AfterEach
+    public void tearDown() {
+    }
+
+
+    //  Kiểm tra tài khoản không tồn tại
+    //  Phải trả về null
+    @Test
+    public void testSignInNull() throws SQLException, NoSuchAlgorithmException{
+        String username = "null";
+        String password = "null";
+        Staff s = signInService.getAccountMD5(username, password);
+        Assertions.assertNull(s);
+    }
+
+    // Kiểm tra tài khoản có tồn tại
+    // Mong muốn trả về Staff khác null
+    @Test
+    public void testSignIn() throws SQLException, NoSuchAlgorithmException {
+        String username = "username2";
+        String password = "12345678";
+        Staff s = signInService.getAccountMD5(username, password);
+        Assertions.assertNotNull(s);
+    }
+
+    // Kiểm tra danh sách Staff có username trùng không
+    @Test
+    public void testUnique() throws SQLException {
+        String query ="SELECT * FROM Staff";
+        PreparedStatement preparedStatement = conn.prepareStatement(query);
+        ResultSet rs = preparedStatement.executeQuery();
+
+        List<String> username = new ArrayList<>();
+        while(rs.next()){
+            username.add(rs.getString("sta_username"));
+        }
+        Set<String> r = new HashSet<>(username);
+
+        Assertions.assertEquals(username.size(), r.size());
+    }
+
+    // Kiểm tra hàm MD5
+    // Chuyền vào chuỗi "123456" mong muốn chuỗi ra "e10adc3949ba59abbe56e057f20f883e"
+    @Test
+    public void testMD5(){
+        String text = "123456";
+        String hope = "e10adc3949ba59abbe56e057f20f883e";
+        Assertions.assertEquals(signInService.MD5(text), hope);
+    }
+
+    // Kiểm tra hàm MD5 khi chuyền ""
+    // Mong muốn trả ra "d41d8cd98f00b204e9800998ecf8427e"
+    @Test
+    public void testMD5Null(){
+        String text = "";
+        String hope = "d41d8cd98f00b204e9800998ecf8427e";
+        Assertions.assertEquals(signInService.MD5(text), hope);
+    }
+}
