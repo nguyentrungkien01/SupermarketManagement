d788491512d66e9a7a812f2c94fb16bdbcc83fc4
vantruong2220 <1951052220truong@gmail.com>
2022-04-04 15:57:44 +0700

    update

--- SupermarketManagement/src/main/java/com/ou/controllers/App.java
+++ SupermarketManagement/src/main/java/com/ou/controllers/App.java
@@ -17,7 +17,7 @@ public class App extends Application {
 
     @Override
     public void start(Stage stage) throws IOException {
-        scene = new Scene(loadFXML("SignIn"));
+        scene = new Scene(loadFXML("sign-in"));
         stage.setScene(scene);
         stage.show();
     }
